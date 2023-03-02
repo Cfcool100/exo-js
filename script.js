d12965f1314950687429fcc91c7158c8e47ef8a6
Youssouf SISSOKOmrsissoko@Sissokos-MacBook-Pro ~ % git config --global user.name Youssouf <cfcool100@gmail.com>
2023-03-02 16:19:16 +0000

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+function contTrue(arr) {
+    var numberOfTrue =0;
+    for (let i = 0; i < arr.length; i++) {
+        if (numberOfTrue == true) {
+            return numberOfTrue+=1;
+        }
+    }
+}
+console.log(contTrue([true,false,true,false,true,false,true,false,true,false]))
