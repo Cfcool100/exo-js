9d2d1bd3bd26d8881875464b3282775cc4d68f6b
Youssouf SISSOKOmrsissoko@Sissokos-MacBook-Pro ~ % git config --global user.name Youssouf <cfcool100@gmail.com>
2023-03-04 23:19:07 +0000

    third exercise

---
+++ absolute_sum.js
@@ -0,0 +1,9 @@
+function getAbsSum(arr) {
+    let sum=0;
+	for (let i = 0; i < arr.length; i++) {
+        Math.abs(i);
+        sum = sum+ Math.abs(arr[i]);
+    }
+    return Math.abs(sum);
+}
+console.log(getAbsSum([1, 2, -81, -12, -5, 10,-6]))
