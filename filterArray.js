913367f308dd95a541789835be7f697eed00c50b
Youssouf SISSOKOmrsissoko@Sissokos-MacBook-Pro ~ % git config --global user.name Youssouf <cfcool100@gmail.com>
2023-03-05 16:01:08 +0000

    reajustment

--- filterArray.js
+++ filterArray.js
@@ -1,10 +1,11 @@
 function filterArray(arr) {
 
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === number) {
-            arr[i];
+        if (typeof arr[i] == 'string') {
+            arr.splice(i,1);
+            i = i - 1;
         }
     }
     return arr[i];
 }
-console.log(filterArray([1, "re", 0, 2, "pq", 10]));
+console.log(filterArray([1, "re", 0, 2, "pq", 10]))
